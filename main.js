9cb2ca204d6a6a496632a32fc56a19eb5098d139
Supercoder <supercoder@AirvonSpercoder.local>
2022-01-17 14:48:40 +0100

    Übungen JavaScript

---
+++ main.js
@@ -0,0 +1,205 @@
+// //////////  Aufgabe 1_1 Console //////////////
+// var a = 12;
+// var b = a * 3;
+// console.log(b);
+// document.write(b);
+
+// console.log(document.URL);
+
+// console.log("Anton");
+// let name1 = "Anton";
+// console.log(name1);
+// let age = 28;
+// console.log(age);
+// let job = "Trainer";
+// console.log(job);
+// var married = true;
+// console.log(married);
+
+// console.log(name1 + "ist " + age + " Jahre alt und ist verheiratet.")
+
+
+
+
+
+// //////////  Aufgabe 1_1 document.write    //////////////
+// document.write("<h1> Hello World! </h1>");
+// document.write("<p> Have a nice day </p>");
+
+// var myText = "Hello again <br><br>";
+// document.write(myText);
+
+// var name1 = "Karin";
+// var nachName = "Löffler";
+// document.write("Name: " + name1 + "<br>");
+// document.write("Nachname: " + nachName);
+
+
+
+
+
+
+
+
+// //////////  Aufgabe 1_4 Popup Boxes //////////////
+
+// window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein.");
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+// let alter = prompt( "Gib mir bitte dein Alter:" );
+// console.log(alter);
+
+// let b = 5;
+// let a = b * 5 -3;
+// alert(a);
+
+// let mann = "Robert Wadlow ist der größte Mann der Welt : ";
+// let grosse = "2,72m";
+// alert (mann + grosse);
+
+
+
+
+
+
+
+// //////////  Aufgabe 1_6 Let Const Var //////////////
+
+// var z = 34;
+// z = 67;
+// console.log(z);
+
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+// const y = 34;
+// y = 67;
+// console.log(y);
+
+
+
+
+
+
+// //////////  Aufgabe 1_5 VARIABLES //////////////
+
+// var carName = "BMW";
+// console.log(carName);
+// var x = 150;
+// console.log(x);
+// var y = 50;
+// console.log(y);
+// var z = x+y;
+// console.log(z);
+
+// let firstName = "John"; let lastName = "Doe"; let age = 35;
+// console.log(firstName + " " + lastName + ", " + age);
+
+
+
+
+
+// //////////  Aufgabe 1_11 String Methods //////////////
+var x = "Hello "
+var y = "World<br>"
+
+var z = x + y;
+document.write(z);
+
+var n = x + " " + y;
+document.write(n);
+
+document.write(x + "World");
+
+var meinString = "Ich bin Erster";
+meinString = meinString + " und ich komme auf Platz zwei."
+console.log(meinString);
+
+
+
+
+
+
+// //////////  Aufgabe 1_12 Arithmetische Operatoren //////////////
+// let score = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score2 = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score2);
+
+// let score3 = 0;
+// score3 = score3 + 10;
+// console.log('Ergebnis: ' + score3);
+
+// let score4 = 0;
+// score4 += 10;
+// console.log('Ergebnis: ' + score4)
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// console.log(zahl);
+// zahl += 1; 
+// console.log(zahl);
+
+// zahl++;
+// console.log("Increment: " + zahl);
+
+// zahl--;
+// console.log("Dekrement: " + zahl)
+
+
+// //////////  Aufgabe 1_13 Arithmetische Wdh //////////////
+// Addition
+// let addition_operator = 1 +1 ;
+// console.log("Addition: " + addition_operator);
+
+// Subtraktion
+// let subtraktion_operator = 2-1;
+// console.log("Subtraktion: " + subtraktion_operator);
+
+// Multiplikation
+// let multiplikation_operator = 2*2;
+// console.log("Multiplikation: " + multiplikation_operator);
+
+// Division
+// let division_operator = 4/2;
+// console.log("Division: " + division_operator);
+
+// Modul: zeigt den Rest an
+// let modulus_operator = 14 % 5;
+// console.log("Modulus: " + modulus_operator);
+
+
+
+
+// //////////  Aufgabe 1_7 //////////////
+
+// let helloWorld = "Hello World";
+// const pi = 3.153;
+// let burjDubai = "828m";
+// let fullName = "Jan Schmidt";
+// let eifelTower = "324m";
+// let camlCase = "camelCase";
+// let kursStatus = true;
+
+// console.log(helloWorld);
+// console.log(pi);
+// console.log(burjDubai);
+// console.log(fullName);
+// console.log(eifelTower);
+// console.log(camlCase);
+// console.log(kursStatus);
+
+
+
+
+
+
+
+
+
+
+
